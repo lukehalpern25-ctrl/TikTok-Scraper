460976e7d25c8727720ef111141844d6e14fd768
Alfred-Onuada <aonuada5@gmail.com>
2025-10-05 12:24:37 +0100

    update

--- src/filters/dedupe.ts
+++ src/filters/dedupe.ts
@@ -1,5 +1,7 @@
+import { readFileSync } from "fs";
 import type { AuthorMeta } from "../interfaces/author";
 import type { ProfileItem } from "../interfaces/profile";
+import path from "path";
 
 const seenHashes = new Set<string>();
 
@@ -19,3 +21,21 @@ export function dedupe(row: AuthorMeta | ProfileItem) {
   return true; // Keep this item
 }
 
+// this loads the creator list from the creator list csv file
+// not using a library because it's a relatively trivial CSV file
+const creatorList = readFileSync(path.join(__dirname, "..", "data", "Creator_URL_Key.csv"), "utf8");
+const creatorListArray = creatorList
+  .split("\n")
+  .map((line) => line.split(",")[0]?.trim())
+  .filter((line) => line !== "");
+
+export function dedupeAgaisntCreatorList(row: ProfileItem) {
+  const inTikTokForm = `${row.authorMeta.profileUrl}`;
+  if (creatorListArray.includes(inTikTokForm)) {
+    console.log(`${row.authorMeta.profileUrl} is in the creator list - filtering out`);
+    return false;
+  }
+
+  console.log(`${row.authorMeta.profileUrl} is not in the creator list - keeping`);
+  return true;
+}
